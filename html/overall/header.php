fcaefd25d4df5db010e0a4c6157dbfb27f826486
Ulises Jeremias <ulisescf.24@gmail.com>
2016-12-25 20:09:06 -0300

    Update header.php

--- html/overall/header.php
+++ html/overall/header.php
@@ -8,7 +8,7 @@
 
     <link rel="apple-touch-icon" href="views/app/img/apple-touch-icon.png">
     <link rel="stylesheet" href="views/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="views/bootstrap-material-design-master/dist/css/bootstrap-material-design.min.css">
+    <link rel="stylesheet" href="views/bootstrap-material-design/css/bootstrap-material-design.min.css">
     <link rel="stylesheet" href="views/fontawesome/css/font-awesome.css">
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="views/app/css/styles.css">
